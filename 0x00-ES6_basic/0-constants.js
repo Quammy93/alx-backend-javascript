c0c835814bd71266d5d2ca5ea2d126838b10182e
root <quammy93@gamail.com>
2023-07-25 15:19:16 +0000

    javascript

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,12 @@
 export function taskFirst() {
-  // Variables declared with 'const'.
   const task = 'I prefer const when I can.';
   return task;
 }
-
 export function getLast() {
   return ' is okay';
 }
-
 export function taskNext() {
-  // Variables declared with 'let'.
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
-
