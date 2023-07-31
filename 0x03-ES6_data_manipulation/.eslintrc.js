a0e7477a16ed8616964e09e168501a168d4fb6ac
root <quammy93@gamail.com>
2023-07-31 20:24:13 +0000

    data_manipulation

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
+
+and…
+Don’t forget to run $ npm install when you have the package.json
+
+Tasks
+0. Basic list of objects
+mandatory
+Create a function named getListStudents that returns an array of objects.
+
+Each object should have three attributes: id (Number), firstName (String), and location (String).
+
+The array contains the following students in order:
+
+Guillaume, id: 1, in San Francisco
+James, id: 2, in Columbia
+Serena, id: 5, in San Francisco
+bob@dylan:~$ cat 0-main.js
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudents());
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 0-main.js 
+[
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+]
